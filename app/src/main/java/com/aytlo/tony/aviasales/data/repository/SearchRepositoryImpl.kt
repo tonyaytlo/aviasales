e9083facd25256ae9a03bed5754232b29c6c00cc
tonyaytlo <tonyaytlo@gmail.com>
2019-06-10 18:58:08 +0300

    added language to reques

--- app/src/main/java/com/aytlo/tony/aviasales/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/com/aytlo/tony/aviasales/data/repository/SearchRepositoryImpl.kt
@@ -2,7 +2,9 @@ package com.aytlo.tony.aviasales.data.repository
 
 import com.aytlo.tony.aviasales.data.model.Cities
 import com.aytlo.tony.aviasales.data.source.remote.AviasalesRepository
+import java.util.*
 
 class SearchRepositoryImpl(private val aviasalesRepository: AviasalesRepository) : SearchRepository {
-    override suspend fun getCities(term: String): Cities = aviasalesRepository.getCities(term)
+    override suspend fun getCities(term: String): Cities =
+        aviasalesRepository.getCities(term, Locale.getDefault().language)
 }
